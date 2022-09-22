61baadf221fa84f19c097127c3c732775ce972dc
Paul Sroczynski <p.sroczynski@gmail.com>
2022-09-22 14:25:00 +0200

    Fix the GitHub action build

--- build-pdf/docker-build.sh
+++ build-pdf/docker-build.sh
@@ -4,10 +4,11 @@ cd "$(dirname "$0")"
 
 cp "$PWD/../resume.md" "$PWD/../build-website/resume.md"
 
-sudo docker run --rm -d --name=jekyll-for-pdf -v="$PWD/../build-website:/srv/jekyll" -p 4001:4001 jekyll/jekyll /bin/bash -c "chmod -R 777 /srv/jekyll && jekyll serve --port 4001 --host 0.0.0.0"
-
-docker run --network host --rm -v="$PWD:/app" buildkite/puppeteer /bin/bash -c "cd app && npm install && npm start -- --url=http://127.0.0.1:4001/resume.html"
-
+docker network create pdf-net
+docker run --network pdf-net --rm -d --name=jekyll-for-pdf -v="$PWD/../build-website:/srv/jekyll" -p 4001:4001 jekyll/jekyll:4.2.0 /bin/bash -c "chmod -R 777 /srv/jekyll && jekyll serve --port 4001"
+serverIp=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jekyll-for-pdf`
+sleep 2s
+docker run --network pdf-net --name=puppeteer -v="$PWD:/app" buildkite/puppeteer /bin/bash -c "cd app && wget http://$serverIp:4001/resume.html && npm install && npm start -- --url=http://$serverIp:4001/resume.html"
 docker stop jekyll-for-pdf
 
 rm "$PWD/../build-website/resume.md"
