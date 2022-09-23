5355414352cd1bb684def36f78f8b185211d074f
Paul Sroczynski <p.sroczynski@gmail.com>
2022-09-23 21:39:16 +0200

    [minor] cleanup the docker container when it's stopped

--- build-pdf/docker-build.sh
+++ build-pdf/docker-build.sh
@@ -8,7 +8,7 @@ docker network create pdf-net
 docker run --network pdf-net --rm -d --name=jekyll-for-pdf -v="$PWD/../build-website:/srv/jekyll" -p 4001:4001 jekyll/jekyll:4.2.0 /bin/bash -c "chmod -R 777 /srv/jekyll && jekyll serve --port 4001"
 serverIp=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jekyll-for-pdf`
 sleep 2s
-docker run --network pdf-net --name=puppeteer -v="$PWD:/app" buildkite/puppeteer /bin/bash -c "cd app && wget http://$serverIp:4001/resume.html && npm install && npm start -- --url=http://$serverIp:4001/resume.html"
+docker run --network pdf-net --rm --name=puppeteer -v="$PWD:/app" buildkite/puppeteer /bin/bash -c "cd app && wget http://$serverIp:4001/resume.html && npm install && npm start -- --url=http://$serverIp:4001/resume.html"
 docker stop jekyll-for-pdf
 
 rm "$PWD/../build-website/resume.md"
